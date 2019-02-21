b40523fde4f48db8f2b4135c97b84549bd7e3218
Marc-Laurent <15917824+Marc-Laurent@users.noreply.github.com>
2019-02-21 14:48:55 +0100

    Add files via upload

---
+++ src/test/book.js
@@ -0,0 +1,29 @@
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import chaiNock from 'chai-nock';
+import chaiAsPromised from 'chai-as-promised';
+import path from 'path';
+import nock from 'nock';
+
+import server from '../server';
+import resetDatabase from '../utils/resetDatabase';
+
+chai.use(chaiHttp);
+chai.use(chaiNock);
+chai.use(chaiAsPromised);
+
+// tout les packages et fonction nescessaire au test sont importé ici, bon courage
+
+// fait les Tests d'integration en premier
+
+chai
+  .request(server)
+  .get('/book')
+  .end((err, res) => {
+    if (err) console.log(err);
+    expect(res).to.have.status(200);
+    expect(res.body).to.be.a('object');
+    expect(res.body.books).to.be.a('array');
+    expect(res.body.books.length).to.equal(0);
+    done();
+  });
